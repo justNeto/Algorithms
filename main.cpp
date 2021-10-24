13f5e18ffe9e3e985526be60a6378dba7a6b77ec
Eduardo Angeles <38306190+Edu4000@users.noreply.github.com>
2021-10-24 15:11:13 -0500

    Template Update

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <ctime>
+#include <chrono>
 #include <math.h>
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
+
 using namespace std;
 
-/*
-Bubble Sort(arr, size)
-		for i=0 to n-i-1
-			for j=0 to n-i-2
-				if arr[j]>arr[j+1]
-					Swap arr[j] and arr[j+1]
+// Insertion Sort
 
-*/
+// Selectionsort
+
+// Bubblesort
 // swap
 // Function that swaps two elements of an array
 template <typename T>
@@ -39,6 +37,7 @@ void bubbleSorted (T unordered [], int size) {
     } 
 }
 
+// Quicksort
 void swapQS(int * posA, int * posB)
 {
 	int aux = *posA;
@@ -72,6 +71,65 @@ void quickSort(int * & arreglo, int low, int high)
 	}
 }
 
+// Mergesort
+int* split(int* & arr, int lowerBound, int upperBound) {
+  int size = upperBound - lowerBound;
+  int *res = new int[size];
+  for (int i = 0; i < size; i++) {
+    res[i] = arr[lowerBound + i];
+  }
+  return res;
+}
+
+void mergeSort(int* & arr, int size) {
+
+  if (size <= 1) {
+    return;
+  }
+
+  int * arr1 = split(arr, 0, size/2);
+  mergeSort(arr1, size/2);
+
+  int * arr2 = split(arr, size/2, size);
+  mergeSort(arr2, size - size/2);
+
+  int size1 = size/2;
+  int size2 = size - size/2;
+
+  int index1 = 0;
+  int index2 = 0;
+  int i = 0;
+
+  while (index1 < size1 && index2 < size2) {
+    if(arr1[index1] < arr2[index2]) {
+      arr[i] = arr1[index1];
+      i++;
+      index1++;
+    } else {
+      arr[i] = arr2[index2];
+      i++;
+      index2++;
+    }
+  }
+
+  while (i < size) {
+    if (index1 < size1) {
+      arr[i] = arr1[index1];
+      index1++;
+    } else {
+      arr[i] = arr2[index2];
+      index2++;
+    }
+    i++;
+  }
+
+  //print(arr, size);
+
+  delete [] arr1;
+  delete [] arr2;
+}
+
+// Countingsort
 void countingSort(int * & arreglo, int size)
 {
     int *arrAux = new int[1000]{};
@@ -102,6 +160,15 @@ void countingSort(int * & arreglo, int size)
     }
 }
 
+/*
+Bubble Sort(arr, size)
+		for i=0 to n-i-1
+			for j=0 to n-i-2
+				if arr[j]>arr[j+1]
+					Swap arr[j] and arr[j+1]
+
+*/
+
 void fill_random(int * & arreglo, int size)
 {
    srand (time(NULL));
@@ -119,39 +186,87 @@ void print_array(int * & arreglo, int size)
 }
 
 int main() {
+	/*
 	// Array declarations
 	int size1, size2, size3, size4, size5, size6;
+
+	// Aray of 100 elements
 	size1 = 100;
 	int * arr1 = new int[size1];
+	fill_random(arr1, size1);
+	//print_array(arr1, size1);
+
+	// Aray of 1000 elements
 	size2 = 1000;
 	int * arr2 = new int[size2];
+	fill_random(arr2, size2);
+	//print_array(arr2, size2);
+
+	// Aray of 10000 elements
 	size3 = 10000;
 	int * arr3 = new int[size3];
+	fill_random(arr3, size3);
+	//print_array(arr3, size3);
+
+	// Aray of 100000 elements
 	size4 = 100000;
 	int * arr4 = new int[size4];
+	fill_random(arr4, size4);
+	//print_array(arr4, size4);
+
+	// Aray of 1000000 elements
 	size5 = 1000000;
 	int * arr5 = new int[size5];
+	fill_random(arr5, size5);
+	//print_array(arr5, size5);
+
+	// Aray of 10000000 elements
 	size6 = 10000000;
 	int * arr6 = new int[size6];
+	fill_random(arr6, size6);
+	//print_array(arr6, size6);
+	*/
 
-	// Clock to measure
-	clock_t t;
-	int f;
-	
-	int size = 11;
-	int * arreglo = new int[10];
-	fill_random(arreglo, 10);
-	print_array(arreglo, 10);
+	cout << "Hello World!" << endl;
+	int size = 8;
+	int * arr = new int[size];
+	fill_random(arr, size);
 
-	cout << "Hello World!\n";
+	cout << "Insertion sort" << endl;
+	print_array(arr,size);
+	mergeSort(arr, size);
+	print_array(arr, size);
+	fill_random(arr, size);
 
-	t = clock();
-	quickSort(arreglo, 0, size -1);
-	t = 2;
-	t = clock() - t;
-	printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+	cout << "Selection sort" << endl;
+	print_array(arr,size);
+	mergeSort(arr, size);
+	print_array(arr, size);
+	fill_random(arr, size);
 
-	print_array(arreglo, 10);
+	cout << "Bubble sort" << endl;
+	print_array(arr,size);
+	bubbleSorted(arr, size);
+	print_array(arr, size);
+	fill_random(arr, size);
+
+	cout << "Quick sort" << endl;
+	print_array(arr,size);
+	quickSort(arr, 0, size-1);
+	print_array(arr, size);
+	fill_random(arr, size);
+
+	cout << "Merge sort" << endl;
+	print_array(arr,size);
+	mergeSort(arr, size);
+	print_array(arr, size);
+	fill_random(arr, size);
+
+	cout << "Counting sort" << endl;
+	print_array(arr,size);
+	countingSort(arr, size);
+	print_array(arr, size);
+	fill_random(arr, size);
 
 	return 0;
 }
